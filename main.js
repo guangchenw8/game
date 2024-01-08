8eafee79d654e79aabeab297922c3eef0dad49ec
guangchenw8 <144932536+guangchenw8@users.noreply.github.com>
2024-01-07 19:35:21 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -1,49 +1,71 @@
-// JS For Project A
+// JS For Quabuble Game
 
 // Set up canvas
 let cnv = document.getElementById('canvas');
 let ctx = cnv.getContext('2d');
 cnv.width = 800;
-cnv.height = 600;
+cnv.height = 800;
 cnv.style.backgroundColor = 'white';
 
 // Global Variables
-
 let mouse = {
   x: 0,
   y: 0,
   pressed: false,
 };
-let plr, enemy, laser, zombie, playbutton;
-let enemyCooldown = 0;
+let playbutton = {
+  x: 200,
+  y: 600,
+  w: 400,
+  h: 125,
+};
+let upgradeammo = {
+  x: 662,
+  y: 200,
+  w: 125,
+  h: 125,
+};
+let upgradebiglaser = {
+  x: 662,
+  y: 350,
+  w: 125,
+  h: 125,
+};
+let upgradefastlaser = {
+  x: 662,
+  y: 500,
+  w: 125,
+  h: 125,
+};
+let plr, enemy1, laser, zombie, powerUp;
 let score = 0;
 let hiScore = 0;
 let laserArray = [];
-let enemyArray = [];
+let enemy1Array = [];
+let powerUpArray = [];
 let gameState = 'start';
 let wIsPressed = false;
 let sIsPressed = false;
 let aIsPressed = false;
 let dIsPressed = false;
 
-// Load frame
-window.addEventListener('load', frame);
-// New frame
-
+// Set the game
 setGame();
 
+// Load frame
+window.addEventListener('load', frame);
+
+// Frame Function
 function frame() {
   ctx.fillStyle = 'white';
   ctx.fillRect(0, 0, cnv.width, cnv.height);
 
-  // Frame content
+  // // Frame content
   runGame();
 
-  // End of Frame
+  // // End of Frame
   requestAnimationFrame(frame);
 }
-
-//  Game Controls
 
 // Event listeners
 document.addEventListener('keydown', keyDownHandler);
@@ -55,7 +77,7 @@ document.addEventListener('mousemove', mouseMoveHandler);
 
 // Event handlers
 
-// Player Controls
+// // Player Controls
 function keyDownHandler(e) {
   if (e.key === 'w') {
     wIsPressed = true;
@@ -96,14 +118,86 @@ function mouseMoveHandler(e) {
   mouse.x = e.clientX - cnvRect.x;
   mouse.y = e.clientY - cnvRect.y;
 }
+// // Click events
 function mouseClickHandler() {
+  // // // Click play button to start
   if (
-    gameState === 'start' &&
+    (gameState === 'start' || gameState === 'lose') &&
     mouse.x > playbutton.x &&
     mouse.x < playbutton.x + playbutton.w &&
     mouse.y > playbutton.y &&
     mouse.y < playbutton.y + playbutton.h
   ) {
     gameState = 'play';
+    setGame();
   }
+  // // // Click upgrades
+  if (gameState === 'play' && plr.xp >= 100) {
+    if (
+      // // // // Ammo upgrade
+      mouse.x > upgradeammo.x &&
+      mouse.x < upgradeammo.x + upgradeammo.w &&
+      mouse.y > upgradeammo.y &&
+      mouse.y < upgradeammo.y + upgradeammo.h
+    ) {
+      plr.upgradeammo += 1;
+      plr.ammo += 50;
+      plr.xp -= 100;
+    } else if (
+      // // // // Big laser upgrade
+      mouse.x > upgradebiglaser.x &&
+      mouse.x < upgradebiglaser.x + upgradebiglaser.w &&
+      mouse.y > upgradebiglaser.y &&
+      mouse.y < upgradebiglaser.y + upgradebiglaser.h
+    ) {
+      plr.upgradebiglaser += 1;
+      plr.xp -= 100;
+    } else if (
+      // // // // Fast laser upgrade
+      mouse.x > upgradefastlaser.x &&
+      mouse.x < upgradefastlaser.x + upgradefastlaser.w &&
+      mouse.y > upgradefastlaser.y &&
+      mouse.y < upgradefastlaser.y + upgradefastlaser.h
+    ) {
+      plr.upgradefastlaser += 1;
+      plr.xp -= 100;
+    }
+    // // // // Remove xp
+  }
+}
+
+// Set Game Function
+function setGame() {
+  // // Reset all object positions and data
+  plr = {
+    x: 380,
+    y: 600,
+    w: 40,
+    h: 40,
+    xSpeed: 0,
+    ySpeed: 0,
+    Accel: 0.2,
+    cooldown: 0,
+    iFrames: 0,
+    health: 100,
+    healthMax: 100,
+    ammo: 50,
+    ammoMax: 50,
+    xp: 0,
+    upgradeammo: 1,
+    upgradebiglaser: 1,
+    upgradefastlaser: 1,
+  };
+  enemy1 = {
+    x: 150 + Math.random() * 460,
+    y: -100,
+    w: 40,
+    h: 40,
+    health: 5,
+    cooldown: 0,
+  };
+  score = 0;
+  laserArray = [];
+  enemy1Array = [];
+  powerUpArray = [];
 }
