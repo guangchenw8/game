9af3e88676790bd3294887b656924180e627dc02
guangchenw8 <144932536+guangchenw8@users.noreply.github.com>
2024-01-12 09:33:10 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -165,39 +165,3 @@ function mouseClickHandler() {
     // // // // Remove xp
   }
 }
-
-// Set Game Function
-function setGame() {
-  // // Reset all object positions and data
-  plr = {
-    x: 380,
-    y: 600,
-    w: 40,
-    h: 40,
-    xSpeed: 0,
-    ySpeed: 0,
-    Accel: 0.2,
-    cooldown: 0,
-    iFrames: 0,
-    health: 100,
-    healthMax: 100,
-    ammo: 50,
-    ammoMax: 50,
-    xp: 0,
-    upgradeammo: 1,
-    upgradebiglaser: 1,
-    upgradefastlaser: 1,
-  };
-  enemy1 = {
-    x: 150 + Math.random() * 460,
-    y: -100,
-    w: 40,
-    h: 40,
-    health: 5,
-    cooldown: 0,
-  };
-  score = 0;
-  laserArray = [];
-  enemy1Array = [];
-  powerUpArray = [];
-}
